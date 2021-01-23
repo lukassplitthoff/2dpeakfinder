6d1a2fcfc52d6329c17ccd0dbc73ade957d17169
LJSplitthoff <lukas.splitthoff@gmail.com>
2021-01-23 11:27:38 +0100

    Create loadimag.py
    start of a great project

---
+++ 2dpeakfinder/loadimag.py
@@ -0,0 +1,10 @@
+"""
+date: 2021-01-23
+author: Alina, Lukas
+
+aim: this function can load the mat matrix and convert it to a python array and create image in python
+"""
+
+def loadimag():
+
+  return
