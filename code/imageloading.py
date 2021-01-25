c0af1bace54ce1f375ba9a8253cb4e29d5677137
Alina <alina.kuliesh@gmail.com>
2021-01-25 16:40:41 +0100

    new function load_image

--- code/imageloading.py
+++ code/imageloading.py
@@ -1,3 +1,14 @@
 '''
-This will be done by Alina
+
+aim: create a function which would load the .mat file 
+date: 25.01.2021
+author: Alina
+
 '''
+
+def load_image():
+    """
+    input: type string (path to the file)
+    output: figure
+    """
+    print('hello')
