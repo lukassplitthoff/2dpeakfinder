143b8551c360dbbc28fc1e60726c06078f5cbff6
lukassplitthoff <Ipvpn_20>
2021-01-26 11:52:35 +0100

    update the doc string

--- code/peakgenerator.py
+++ code/peakgenerator.py
@@ -7,12 +7,16 @@ def gaussion_1d(x, amp, cen, norm):
     """
     Calculate 1d gaussian function for given x
 
-    param: x
-    param: amp
-    param: cen
-    param: norm
+    Parameter
+    ---------
+    x: float
+    amp: float
+    cen: float
+    norm: float
 
-    return: function
+    Returns
+    -------
+    function: float
     """
 
     return amp * np.exp(-(x - cen) ** 2 / (2 * norm ** 2))
@@ -20,30 +24,41 @@ def gaussion_1d(x, amp, cen, norm):
 
 def gaussian_2d(x, y, amp, cen, norm):
     """
-    Calculate 2d gaussian function for given x,y
+    Calculate 2d gaussian function for given x and y
 
-    param: x - 1d np array
-    param: y - 1d np array
-    param: amp - 2d list
-    param: cen - 2d list
-    param: norm - 2d list
+    Parameter
+    ---------
+    x: float
+    y: float
+    amp: float, list
+    cen: float, list
+    norm: float, list
 
-    return: scalar function
+    Returns
+    -------
+    function: float
     """
     x, y = np.meshgrid(x, y)
     return gaussion_1d(x, amp[0], cen[0], norm[0]) * gaussion_1d(y, amp[1], cen[1], norm[1])
 
 
 def inspector(matrix):
-    """"
-    Make simple 2D plot without any extras
+    """
+    Calculate 2d gaussian function for given x and y
 
-    param: matrix - 2d np array
+    Parameter
+    ---------
+    matrix: float, array
+
+    Returns
+    -------
+    figure
     """
     fig, axes = plt.subplots()
     im = axes.imshow(matrix, cmap="viridis")
     fig.colorbar(im)
     fig.show()
+    pass
 
 
 def _example():
