21a4e6507817660d9269516b5b3158b259c2d177
lukassplitthoff <Ipvpn_20>
2021-01-27 11:04:53 +0100

    try to resolve conflict with main

--- code/imageloading.py
+++ code/imageloading.py
@@ -1,36 +1,26 @@
 '''
 <<<<<<< HEAD
 aim: create a function which would load the .mat file
-=======
-aim: create a function which would load the .mat file 
->>>>>>> origin/peakgenerator
+
 date: 25.01.2021
 author: Alina
 '''
 import scipy.io as sio
 
 def load_image(path):
-<<<<<<< HEAD
-    """Load the image and display it
-=======
-    """Load the image and display it 
 
->>>>>>> origin/peakgenerator
+    """Load the image and display it
+
+
     Parameters
     ----------
     path : string
         path to the file
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/peakgenerator
     Returns
     -------
     Im : numpy array
     """
     Im = sio.loadmat(path)
-<<<<<<< HEAD
+
     return Im['Im']
-=======
-    return Im['Im']
->>>>>>> origin/peakgenerator
